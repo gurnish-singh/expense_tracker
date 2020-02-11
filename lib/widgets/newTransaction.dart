8db7acfb8969aae4e44f09ec9e4d838237f7b83d
gurnish-singh <gsingh11_be17@thapar.edu>
2020-02-12 00:42:36 +0530

    fixed state issues
    fixed state issues

--- lib/widgets/newTransaction.dart
+++ lib/widgets/newTransaction.dart
@@ -1,23 +1,31 @@
-
-
 import 'package:flutter/material.dart';
 
 ///////it contains  the transaction text fields container and the flat button for adding new transactions///////////////
-class NewTransaction extends StatelessWidget {
-  final title = TextEditingController();
-  final amount = TextEditingController();
+class NewTransaction extends StatefulWidget {
   final Function addNew;
-  void Submit() {
-    if ((title.text).isEmpty || (double.parse(amount.text))<=0) {
+  NewTransaction(this.addNew);
+  @override
+  _NewTransactionState createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final title = TextEditingController();
+  final amount = TextEditingController(); //controllers for the text fields
+
+  void submit() {
+    if ((title.text).isEmpty || (double.parse(amount.text)) <= 0) {
       return;
     }
-      addNew(title.text, double.parse(amount.text));
-      //addNew function is in userTransaction.
-      // amount should be always double
-      /// to convert string to double do double.parse('string')
+    widget.addNew(
+      title.text,
+      double.parse(amount.text),
+    );
+    //addNew function is in userTransaction.
+    // amount should be always double
+    /// to convert string to double do double.parse('string')
+  Navigator.of(context).pop();//used to close the topmost section of thr screen
   }
 
-  NewTransaction(this.addNew); //constructor to calll
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -34,6 +42,7 @@ class NewTransaction extends StatelessWidget {
                 //title = value;
                 // },
                 controller: title,
+                keyboardType: TextInputType.text,
               ),
               TextField(
                   decoration: InputDecoration(labelText: 'Amount'),
@@ -43,14 +52,14 @@ class NewTransaction extends StatelessWidget {
                   controller: amount,
                   keyboardType:
                       TextInputType.number, //only a number(double) to be typed
-                  onSubmitted: (val) => Submit() //pointer to that function
+                  onSubmitted: (val) => submit() //pointer to that function
                   //// IMP onSubmitted will always return a string so there is a need to fake it that we are receiving it
                   ///although we are not using it here
                   ),
               FlatButton(
                 child: Text('Add transaction'),
                 onPressed: () {
-                  Submit();
+                  submit();
                 },
                 textColor: Colors.purple[200],
               )
